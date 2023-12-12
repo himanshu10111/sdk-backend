b1ff3999ec0590c7548009a3b80a9999ed0e14ca
himanshu <himanshudoye42@gmail.com>
2023-12-12 17:30:59 +0530

    added code v1

---
+++ src/main/java/com/ssk/SdkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdkBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ssk/controller/NotificationController.java
@@ -0,0 +1,18 @@
+package com.ssk.controller;
+
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    @CrossOrigin(origins = "http://localhost:3000") // Replace with the origin you want to allow
+    @GetMapping("/push-notification")
+    public String getPushNotification() {
+        // Mock notification message
+        return "You have a new notification!";
+    }
+}
